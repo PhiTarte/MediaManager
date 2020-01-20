ae1d1759c6ef1721a99da7adbdf996bdf6875dc2
Unknown <tarteserviceinformatique@sfr.fr>
2020-01-20 16:56:12 +0100

    begin to fetch a route of the API

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const opt={
+    method:'GET'
+  }
+  fetch("/localhost:8000/realisateur/hitchcock",opt)
+  .then(res=>{
+    return res
+  })
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +27,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
+  
 }
 
 export default App;
